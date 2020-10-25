bfee3919f562bf1aae10bd641542de2c79098695
tatsuya828828 <kkjy011894@gmail.com>
2020-10-25 12:42:51 +0900

    jsを追加

---
+++ profile.js
@@ -0,0 +1,27 @@
+$(function(){
+    ScrollReveal().reveal('.move-left', { delay: 200, distance: '500px', origin: 'left' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right1', { delay: 400, distance: '500px', origin: 'right' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right2', { delay: 600, distance: '500px', origin: 'right' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right3', { delay: 800, distance: '500px', origin: 'right' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right4', { delay: 1000, distance: '500px', origin: 'right' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right5', { delay: 1200, distance: '500px', origin: 'right' });
+});
+
+$(function(){
+    ScrollReveal().reveal('.move-right6', { delay: 1400, distance: '500px', origin: 'right' });
+});
